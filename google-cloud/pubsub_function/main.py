bf3c738e188125835bd89b0ccf17bdd6e14a01e2
James Watkinson <62016404+jawatDSQ@users.noreply.github.com>
2020-04-29 21:11:42 +0100

    Aligned to save_plan.py

--- google-cloud/pubsub_function/main.py
+++ google-cloud/pubsub_function/main.py
@@ -1,5 +1,7 @@
-from houston.plugin.gcp import GCPHouston as Houston
+"""Example Houston Stage"""
+
 import os
+from houston.plugin.gcp import GCPHouston as Houston
 
 KEY = os.getenv('API_KEY')
 
@@ -13,7 +15,7 @@ def main(event, context):
     """
 
     # initialise Houston client
-    h = Houston(plan="houston-quickstart", api_key=KEY)
+    h = Houston(plan="houston-quickstart-jwatkinson", api_key=KEY)
 
     # get instructions from the message that triggered this function
     stage_info = h.extract_stage_information(event["data"])
@@ -22,40 +24,33 @@ def main(event, context):
     if stage == "start":
         # start a new mission and get an ID for it
         mission_id = h.create_mission()
+        print(f"Starting a new mission with id: {mission_id}")
     else:
         # otherwise continue the existing mission
         mission_id = stage_info['mission_id']
 
     # call houston to start the stage
-    h.start_stage(stage, mission_id=mission_id)
+    stage_detail = h.start_stage(stage, mission_id=mission_id)
 
     #
     #
     #
 
     # run the main process
-    if 'upload-file' in stage:
-        upload_file(stage_info['file_name'])
+    if 'file_location' in stage_detail["params"]:
+        upload_file(stage_detail["params"]["file_location"])
 
-    elif 'run-query' in stage:
-        run_query(stage_info['query_name'])
+    elif 'query_name' in stage_detail["params"]:
+        run_query(stage_detail["params"]["query_name"])
 
-    elif 'build-report' in stage:
-        build_report(stage_info['source_table'])
-
-    #else:
-    #    raise ValueError(f"Didn't know what task to run for stage: '{stage}'")
-
-    #
-    #
-    #
+    elif 'source_table' in stage_detail["params"]:
+        build_report(stage_detail["params"]["source_table"])
 
     # call houston to end the stage - response from houston will tell us which stages are next
     response = h.end_stage(stage, mission_id)
-
     # trigger the next stage(s)
     h.call_stage_via_pubsub(response, mission_id)
-
+    return
 
 #
 # tasks
@@ -63,12 +58,15 @@ def main(event, context):
 
 
 def upload_file(file_location):
+    print(f"Uploading File: {file_location}")
     pass
 
 
 def run_query(query_name):
+    print(f"Running Query: {query_name}")
     pass
 
 
 def build_report(source_table):
+    print(f"Building Report: {source_table}")
     pass
